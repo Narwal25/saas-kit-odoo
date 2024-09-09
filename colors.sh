6f8e5eacdc5cb409b7f5af02081019bf77bfd75c
Narwal25 <ritiknarwal588@gmail.com>
2024-09-09 18:05:38 +0530

    Added Colors to enchance UX

---
+++ colors.sh
@@ -0,0 +1,16 @@
+# Colours
+export black="\033[0;30m"  # Black
+export red="\033[31m"      # Red
+export green="\033[0;32m"  # Green
+export yellow="\033[0;33m" # Yellow
+export blue="\033[0;34m"   # Blue
+export purple="\033[0;35m" # Purple
+export cyan="\033[0;36m"   # Cyan
+export white="\033[0;37m"  # White
+export bold="\033[1m"
+export uline="\033[4m"
+export reset="\033[0m"
+export expand_bg="\033[K"
+export blue_bg="\033[0;104m${expand_bg}"
+export red_bg="\033[0;101m${expand_bg}"
+export green_bg="\033[0;102m${expand_bg}"
