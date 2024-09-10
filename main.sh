600e7e51e89dae0708842ddc60c101ccb8139856
Narwal25 <ritiknarwal588@gmail.com>
2024-09-10 11:24:46 +0530

    minor changes

--- main.sh
+++ main.sh
@@ -131,7 +131,7 @@ run_non_interactive() {
 
 run_remote_server() {
     ssh_setup_remote_server
-    scp -ruv remote_server.sh ${remote_server_ssh_user}@${remote_server_ip}:/tmp/remote_server.sh
+    scp -r remote_server.sh ${remote_server_ssh_user}@${remote_server_ip}:/tmp/remote_server.sh
     sshremoteserver 'source /tmp/remote_server.sh'
     sshremoteserver 'packages_install_remote_server'
     sshremoteserver 'python_packages_install_remote_server'
