0c7ede69cbdf3728f858d9f9c324a06bb46b3ae4
Narwal25 <ritiknarwal588@gmail.com>
2024-09-19 16:32:12 +0530

    Modified odoo installation

--- Odoo17_Installation.sh
+++ Odoo17_Installation.sh
@@ -45,6 +45,7 @@ echo "[options]
    db_password = odoo
    addons_path = /opt/odoo/odoo/addons
    logfile = /var/log/odoo/odoo.log
+   proxy_mode = True
    " > $odoo_conf_file
 
 sudo chown $odoo_username: $odoo_conf_file
@@ -80,28 +81,75 @@ systemctl start nginx
 
 touch /etc/nginx/sites-enabled/${server_domain}.conf
 
-echo "server {
-    listen 80;
-    listen [::]:80;
-    server_name ${server_domain};
-    return 301 https://\$host\$request_uri;
+echo "#odoo server
+upstream odoo {
+  server 127.0.0.1:8069;
+}
+upstream odoochat {
+  server 127.0.0.1:8072;
+}
+map \$http_upgrade \$connection_upgrade {
+  default upgrade;
+  ''      close;
+}
+
+# http -> https
+server {
+  listen 80;
+  server_name ${server_domain};
+  rewrite ^(.*) https://\$host\$1 permanent;
 }
 
 server {
-    listen 443 ssl;
-    listen [::]:443 ssl;
-    server_name ${server_domain};
+  listen 443 ssl;
+  server_name ${server_domain};
+  proxy_read_timeout 720s;
+  proxy_connect_timeout 720s;
+  proxy_send_timeout 720s;
 
-    ssl_certificate /etc/letsencrypt/live/${server_domain}/fullchain.pem;
-    ssl_certificate_key /etc/letsencrypt/live/${server_domain}/privkey.pem;
+  # SSL parameters
+  ssl_certificate /etc/letsencrypt/live/${server_domain}/fullchain.pem;
+  ssl_certificate_key /etc/letsencrypt/live/${server_domain}/privkey.pem;
+  ssl_session_timeout 30m;
+  ssl_protocols TLSv1.2;
+  ssl_ciphers ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384;
+  ssl_prefer_server_ciphers off;
 
-    location / {
-        proxy_pass http://localhost:8069;
-        proxy_set_header Host \$host;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto \$scheme;
-    }
+  # log
+  access_log /var/log/nginx/odoo.access.log;
+  error_log /var/log/nginx/odoo.error.log;
+
+  # Redirect websocket requests to odoo gevent port
+  location /websocket {
+    proxy_pass http://odoochat;
+    proxy_set_header Upgrade \$http_upgrade;
+    proxy_set_header Connection \$connection_upgrade;
+    proxy_set_header X-Forwarded-Host \$http_host;
+    proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+    proxy_set_header X-Forwarded-Proto \$scheme;
+    proxy_set_header X-Real-IP \$remote_addr;
+
+    add_header Strict-Transport-Security 'max-age=31536000; includeSubDomains';
+    #proxy_cookie_flags session_id samesite=lax secure;  # requires nginx 1.19.8
+  }
+
+  # Redirect requests to odoo backend server
+  location / {
+    # Add Headers for odoo proxy mode
+    proxy_set_header X-Forwarded-Host \$http_host;
+    proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+    proxy_set_header X-Forwarded-Proto \$scheme;
+    proxy_set_header X-Real-IP \$remote_addr;
+    proxy_redirect off;
+    proxy_pass http://odoo;
+
+    add_header Strict-Transport-Security 'max-age=31536000; includeSubDomains';
+    #proxy_cookie_flags session_id samesite=lax secure;  # requires nginx 1.19.8
+  }
+
+  # common gzip
+  gzip_types text/css text/scss text/plain text/xml application/xml application/json application/javascript;
+  gzip on;
 }" > /etc/nginx/sites-enabled/${server_domain}.conf
 
 nginx_test_output=$(nginx -t 2>&1)
