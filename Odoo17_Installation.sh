1b10c87e7c544bbd06454d734da25d86c3ed97c7
Narwal25 <ritiknarwal588@gmail.com>
2024-12-03 15:36:02 +0530

    updated odoo installation script

--- Odoo17_Installation.sh
+++ Odoo17_Installation.sh
@@ -1,85 +1,125 @@
+#!/bin/bash
+
+# Define Variables
+odoo_conf_file="/etc/odoo.conf"
+odoo_username="odoo"  # Odoo Username (change as needed)
+odoo_home="/opt/odoo"  # Odoo Home Directory (change as needed)
+server_domain="odoo.narwal25.site"
+odoo_version="17.0"  # Variable for Odoo version
 source .env
 
-sudo apt-get update -y
-sudo apt-get install -y python3-pip
-sudo apt-get install python-dev python3-dev libxml2-dev libxslt1-dev zlib1g-dev libsasl2-dev libldap2-dev build-essential libssl-dev libffi-dev libmysqlclient-dev libjpeg-dev libpq-dev libjpeg8-dev liblcms2-dev libblas-dev libatlas-base-dev
-sudo apt-get install -y npm
-sudo ln -s /usr/bin/nodejs /usr/bin/node
-sudo npm install -g less less-plugin-clean-css
-sudo apt-get install -y node-less
-sudo apt install python3-pip libldap2-dev libpq-dev libsasl2-dev -y
-sudo apt-get install postgresql -y
-sudo npm install -g rtlcss
+# Enable Debugging and Exit on Error
+set -xe
 
+# Function to Install System Packages
+install_system_packages() {
+    sudo apt-get update -y
+    sudo apt-get install -y python3-pip
+    sudo apt-get install -y python3-dev libxml2-dev libxslt1-dev zlib1g-dev libsasl2-dev libldap2-dev build-essential libssl-dev libffi-dev libmysqlclient-dev libjpeg-dev libpq-dev libjpeg8-dev liblcms2-dev libblas-dev libatlas-base-dev
+    sudo apt-get install -y npm
+    sudo apt install python3-cffi libldap2-dev libpq-dev libsasl2-dev -y
+    sudo apt install xfonts-75dpi xfonts-base -y
+    sudo apt-get install postgresql -y
+    sudo apt install nginx -y
+}
 
-su - postgres -c "psql -U postgres -c \"CREATE ROLE $odoo_username WITH NOCREATEROLE NOSUPERUSER CREATEDB LOGIN;\""
-su - postgres -c "psql -U postgres -c \"ALTER ROLE $odoo_username WITH PASSWORD 'odoo';\""
-su - postgres -c "psql -U postgres -c \"ALTER USER $odoo_username WITH SUPERUSER;;\""
+# Function to Install Node.js and Dependencies
+install_nodejs_and_dependencies() {
+    if [ ! -f "/usr/bin/node" ]; then
+      sudo ln -s /usr/bin/nodejs /usr/bin/node  
+    fi
+    sudo npm install -g less less-plugin-clean-css
+    sudo apt-get install -y node-less
+    sudo npm install -g rtlcss
+}
 
-sudo adduser --system --home=/opt/odoo --group $odoo_username
-sudo apt-get install git
+# Function to Setup PostgreSQL Database and User
+setup_postgresql() {
+    role_exists=$(su - postgres -c "psql -tAc \"SELECT 1 FROM pg_roles WHERE rolname = '$odoo_username'\"")
+    if [ "$role_exists" = "1" ]; then
+        echo "Role '$odoo_username' already exists."
+    else
+      su - postgres -c "psql -U postgres -c \"CREATE ROLE $odoo_username WITH NOCREATEROLE NOSUPERUSER CREATEDB LOGIN;\""
+    fi
+    su - postgres -c "psql -U postgres -c \"ALTER ROLE $odoo_username WITH PASSWORD 'odoo';\""
+    su - postgres -c "psql -U postgres -c \"ALTER USER $odoo_username WITH SUPERUSER;\""
+}
 
-su - odoo -s /bin/bash -c "git clone https://www.github.com/odoo/odoo --depth 1 --branch 17.0 --single-branch /opt/odoo/odoo"
+# Function to Setup Odoo User and Install Odoo from Git
+setup_odoo_user_and_install() {
+    sudo adduser --system --home=$odoo_home --group $odoo_username
+    sudo apt-get install git
+    if [ -d "$odoo_home/odoo" ]; then
+      CURRENT_TIME=$(date | cut -d" " -f5)
+      mv $odoo_home/odoo $odoo_home/odoo_backup_"${CURRENT_TIME}"
+    fi
+    su - $odoo_username -s /bin/bash -c "git clone https://www.github.com/odoo/odoo --depth 1 --branch $odoo_version --single-branch $odoo_home/odoo"
+}
 
-sudo apt install python3-cffi -y
-sudo pip install -r /opt/odoo/odoo/requirements.txt
+# Function to Install Python Requirements for Odoo
+install_python_requirements() {
+    sudo sed -i "s/gevent==21.8.0/gevent==21.12.0/g"  $odoo_home/odoo/requirements.txt
+    sudo pip install -r $odoo_home/odoo/requirements.txt
+}
 
-apt install xfonts-75dpi xfonts-base -y 
-apt --fix-broken install -y
-wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-3/wkhtmltox_0.12.6.1-3.jammy_amd64.deb
-dpkg -i wkhtmltox_0.12.6.1-3.jammy_amd64.deb 
-sudo apt install -f
+# Function to Install wkhtmltopdf
+install_wkhtmltopdf() {
+    wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-3/wkhtmltox_0.12.6.1-3.jammy_amd64.deb
+    dpkg -i wkhtmltox_0.12.6.1-3.jammy_amd64.deb
+    sudo apt install -f
+}
 
-sudo cp /opt/odoo/odoo/debian/odoo.conf $odoo_conf_file
-sudo touch $odoo_conf_file
+# Function to Create and Configure Odoo Configuration File
+create_odoo_config() {
+    sudo cp $odoo_home/odoo/debian/odoo.conf $odoo_conf_file
+    sudo touch $odoo_conf_file
 
-
-echo "[options]
+    echo "[options]
    ; This is the password that allows database operations:
    admin_passwd = admin
    db_host = False
    db_port = False
    db_user = $odoo_username
    db_password = odoo
-   addons_path = /opt/odoo/odoo/addons
+   addons_path = $odoo_home/odoo/addons
    logfile = /var/log/odoo/odoo.log
    proxy_mode = True
    " > $odoo_conf_file
 
-sudo chown $odoo_username: $odoo_conf_file
-sudo chmod 640 $odoo_conf_file
+    sudo chown $odoo_username: $odoo_conf_file
+    sudo chmod 640 $odoo_conf_file
+}
 
-sudo mkdir /var/log/odoo
-sudo chown odoo:root /var/log/odoo
+# Function to Setup Odoo Service
+setup_odoo_service() {
+    sudo mkdir /var/log/odoo
+    sudo chown $odoo_username:root /var/log/odoo
+    sudo touch /etc/systemd/system/odoo.service
 
-sudo touch /etc/systemd/system/odoo.service
-
-
-echo "[Unit]
-   Description=Odoo17
+    echo "[Unit]
+   Description=Odoo$odoo_version
    Documentation=http://www.odoo.com
    [Service]
    # Ubuntu/Debian convention:
    Type=simple
    User=$odoo_username
-   ExecStart=/opt/odoo/odoo/odoo-bin -c $odoo_conf_file
+   ExecStart=$odoo_home/odoo/odoo-bin -c $odoo_conf_file
    [Install]
    WantedBy=default.target
    " > /etc/systemd/system/odoo.service
 
-sudo chmod 755 /etc/systemd/system/odoo.service
-sudo chown root: /etc/systemd/system/odoo.service
+    sudo chmod 755 /etc/systemd/system/odoo.service
+    sudo chown root: /etc/systemd/system/odoo.service
 
+    sudo systemctl enable odoo.service
+    sudo systemctl start odoo.service
+}
 
-sudo systemctl enable odoo.service
-sudo systemctl start odoo.service
+# Function to Configure Nginx for Odoo
+configure_nginx() {
+    touch /etc/nginx/sites-enabled/${server_domain}.conf
 
-sudo apt install nginx -y
-systemctl start nginx
-
-touch /etc/nginx/sites-enabled/${server_domain}.conf
-
-echo "#odoo server
+    echo "#odoo server
 upstream odoo {
   server 127.0.0.1:8069;
 }
@@ -128,7 +168,6 @@ server {
     proxy_set_header X-Real-IP \$remote_addr;
 
     add_header Strict-Transport-Security 'max-age=31536000; includeSubDomains';
-    #proxy_cookie_flags session_id samesite=lax secure;  # requires nginx 1.19.8
   }
 
   # Redirect requests to odoo backend server
@@ -142,7 +181,6 @@ server {
     proxy_pass http://odoo;
 
     add_header Strict-Transport-Security 'max-age=31536000; includeSubDomains';
-    #proxy_cookie_flags session_id samesite=lax secure;  # requires nginx 1.19.8
   }
 
   # common gzip
@@ -150,10 +188,26 @@ server {
   gzip on;
 }" > /etc/nginx/sites-enabled/${server_domain}.conf
 
-nginx_test_output=$(nginx -t 2>&1)
+    nginx_test_output=$(nginx -t 2>&1)
 
-if [ $? -eq 0 ]; then
-    sudo systemctl restart nginx
-else
-    echo "$nginx_test_output"
-fi
+    if [ $? -eq 0 ]; then
+        sudo systemctl restart nginx
+    else
+        echo "$nginx_test_output"
+    fi
+}
+
+# Main Execution
+install_odoo_stack(){
+    install_system_packages
+    install_nodejs_and_dependencies
+    setup_postgresql
+    setup_odoo_user_and_install
+    install_python_requirements
+    install_wkhtmltopdf
+    create_odoo_config
+    setup_odoo_service
+    configure_nginx
+}
+
+install_odoo_stack
